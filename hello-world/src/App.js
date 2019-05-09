28d4feacfa1085627814d1474f4778d9688b7bc9
Lexavil <aviles.esp@gmail.com>
2019-05-09 00:16:02 -0500

    primer prueba

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -7,15 +7,26 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <img src={"https://scontent.fgdl2-1.fna.fbcdn.net/v/t1.15752-9/60013714_300431670886622_5160639309086195712_n.png?_nc_cat=104&_nc_ht=scontent.fgdl2-1.fna&oh=358182c5446e8686d8060566643970f0&oe=5D5C73BB"} className="App-logo" alt="logo" />
+          </div>
+          <h2>Selecciona alguna de las siguientes opciones</h2>
+          <form>
+  <label>
+   
+    <input type="text" placeholder="Ingresa el nombre del cliente" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+      
+        
+<button class="button button1">Café</button>
+<button class="button button2">Chapata</button>
+<button class="button button3">Soda Italiana</button>
+<button class="button button4">Ensalada</button>
+<button class="button button5">Hamburguesa</button>
       </div>
     );
   }
 }
-
+      
 export default App;
